7efc3f6b6e45b275934a21adc676042ab37b4c59
Photogrammer <khsj15@khu.ac.kr>
2023-06-03 20:08:07 +0900

    FEAT: validationOptions.constant.ts
    - class-validator, class-transformer 를 위한 validation pipe 구성

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { winstonLogger } from './common/interceptors/logger/winston.utill';
+import { winstonLogger } from './common/middlewares/logger/winston.util';
+import { ValidationPipe } from '@nestjs/common';
+import { ValidationOptions } from './common/pipes/validationPipe/validationOptions.constant';
+import { PrismaClientExceptionFilter } from './common/filters/prismaClientException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: winstonLogger,
   });
+  app.useGlobalPipes(new ValidationPipe(ValidationOptions));
+  app.useGlobalFilters(new PrismaClientExceptionFilter());
+  // 추후 변경할 것
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
