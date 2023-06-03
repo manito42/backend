2d0b9ce1f048dbad87ab6f5026ff404875fa5b75
Photogrammer <khsj15@khu.ac.kr>
2023-06-03 20:11:41 +0900

    FEAT: JWT 인증 가드

---
+++ src/common/guards/jwt/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtConfigService } from '../../../config/jwt/config.service';
+import { JwtPayloadInterface } from '../../interfaces/jwt/jwtPayload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  // passport-jwt strategy 를 통해서 jwt token 을 검증 & payload 를 추출
+  constructor(private jwtConfigService: JwtConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConfigService.secret,
+    });
+  }
+
+  // 검증된 JWT 토큰인 경우 존재하는 유저로 확정하고, 접근을 허용합니다.
+  async validate(payload: JwtPayloadInterface) {
+    return payload;
+  }
+}
