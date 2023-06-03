e8886e72326cb2f07256ee61b508a426be6f945b
Photogrammer <khsj15@khu.ac.kr>
2023-06-03 20:59:05 +0900

    FIX: HOME Module Dependency

--- src/modules/home/home.module.ts
+++ src/modules/home/home.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { HomeController } from './home.controller';
 import { HomeService } from './home.service';
 import { MentorProfileModule } from '../../models/mentorProfile/mentorProfile.module';
+import { MentorProfileService } from '../../models/mentorProfile/mentorProfile.service';
+import { PrismaService } from '../../database/services/prisma.service';
 
 @Module({
   imports: [MentorProfileModule],
   controllers: [HomeController],
-  providers: [HomeService],
+  providers: [HomeService, MentorProfileService, PrismaService],
 })
 export class HomeModule {}
