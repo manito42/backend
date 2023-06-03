a3cf0dcee7a258b9f941d544c848e02d2497b0da
Photogrammer <khsj15@khu.ac.kr>
2023-06-03 20:23:30 +0900

    CHORE: APP MODULE 의존성 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,37 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './database/services/prisma.service';
 import { UserModule } from './models/user/user.module';
-import { LoggerMiddleware } from './common/middleswares/logger/logger.middleware';
+import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware';
+import { HashtagModule } from './models/hashtag/hashtag.module';
+import { MentorFeedbackModule } from './models/mentorFeedback/mentorFeedback.module';
+import { MenteeFeedbackModule } from './models/menteeFeedback/menteeFeedback.module';
+import { MentorProfileModule } from './models/mentorProfile/mentorProfile.module';
+import { ReservationModule } from './models/reservation/reservation.module';
+import { CategoryModule } from './models/category/category.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { FtConfigModule } from './config/ft/config.module';
+import { JwtConfigModule } from './config/jwt/config.module';
+import { AppConfigModule } from './config/app/config.module';
+import { SearchModule } from './modules/search/search.module';
+import { HomeModule } from './modules/home/home.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    CategoryModule,
+    HashtagModule,
+    MenteeFeedbackModule,
+    MentorFeedbackModule,
+    MentorProfileModule,
+    ReservationModule,
+    UserModule,
+    FtConfigModule,
+    JwtConfigModule,
+    AppConfigModule,
+    SearchModule,
+    HomeModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService, Logger],
 })
