8d928fe1a0c110eeda622650012496e7a27826d3
photogrammer <khsj15@khu.ac.kr>
2023-07-19 17:43:03 +0900

    update: api guard in Hashtags

--- src/models/hashtag/hashtag.controller.ts
+++ src/models/hashtag/hashtag.controller.ts
@@ -9,12 +9,14 @@ import {
   Post,
   Query,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import { HashtagService } from './hashtag.service';
 import { HashtagGetResponseDto } from './dto/response/hashtagGetResponse.dto';
 import { HashtagCreatePayloadDto } from './dto/request/hashtagCreatePayload.dto';
 import { GetHashtagsQueryDto } from './dto/request/hashtagQuery.dto';
 import { Response } from 'express';
+import { JwtGuard } from '../../common/guards/jwt/jwt.guard';
 
 @Controller('/hashtags')
 export class HashtagController {
@@ -37,6 +39,7 @@ export class HashtagController {
    * @note: 일반적인 상황에서는 이미 생성된 객체에 대해서 409 를 반환하는 것이 맞겠으나, 협의 이후 이미 존재할 경우 200을 response 하도록했습니다.
    */
   @Post('/')
+  @UseGuards(JwtGuard)
   async create(
     @Body() payload: HashtagCreatePayloadDto,
     @Res({ passthrough: true }) res: Response,
