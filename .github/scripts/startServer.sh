429c09828744b4970925c8d7fbdc1085e9799091
myukang <myunghwan0421@gmail.com>
2023-08-19 00:03:52 +0900

    :bricks: fix: prisma deploy
    - deployment 수행 후 appSpec.yml에서 수행하는 테스트. private database vpn으로 우회할 필요성 있어보임.

--- .github/scripts/startServer.sh
+++ .github/scripts/startServer.sh
@@ -5,4 +5,8 @@ screen
 # Start the server
 echo "Starting server..."
 cd /hoem/ubuntu
+
+# deploy는 현재 ./prisma/migrations 폴더에 있는 마이그레이션 파일들을 읽어서 DB에 적용합니다.
+npx prisma migrate deploy
+
 node dist/main
