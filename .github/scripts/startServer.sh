ba8a66d4e884a044a4f44f1afd32781e228a6838
myukang <myunghwan0421@gmail.com>
2023-08-19 02:48:01 +0900

    :bricks: fix: change executable

--- .github/scripts/startServer.sh
+++ .github/scripts/startServer.sh
@@ -5,6 +5,6 @@ echo "Starting server..."
 cd /home/ubuntu
 
 # deploy는 현재 ./prisma/migrations 폴더에 있는 마이그레이션 파일들을 읽어서 DB에 적용합니다.
-/home/ubuntu/.nvm/versions/node/v18.16.0/bin/npx prisma migrate deploy
+npx prisma migrate deploy
 
-/home/ubuntu/.nvm/versions/node/v18.16.0/bin/npm run start:prod
+npm run start:prod
