a3e0f330e4d9a9001b8fdf363135d426ca14631b
myukang <myunghwan0421@gmail.com>
2023-08-19 03:48:37 +0900

    :bricks: fix: install prisma

--- .github/scripts/startServer.sh
+++ .github/scripts/startServer.sh
@@ -3,6 +3,9 @@
 echo "change directory"
 cd /home/ubuntu
 
+# prisma migrate를 실행하기 위해 prisma를 설치합니다.
+npm install prisma --save-dev
+
 # deploy는 현재 ./prisma/migrations 폴더에 있는 마이그레이션 파일들을 읽어서 DB에 적용합니다.
 echo "Deploying prisma..."
 npx prisma migrate deploy
