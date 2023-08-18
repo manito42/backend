678201c97af6e60562df159b74dc249a3a75121e
myukang <myunghwan0421@gmail.com>
2023-08-19 00:26:03 +0900

    :bricks: fix: screen error
    [sceen error](https://serverfault.com/questions/825280/after-launching-sh-script-appearing-must-be-connected-to-a-terminal)

--- .github/scripts/startServer.sh
+++ .github/scripts/startServer.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-screen
+screen -dm /bin/bash
 
 # Start the server
 echo "Starting server..."
-cd /hoem/ubuntu
+cd /home/ubuntu
 
 # deploy는 현재 ./prisma/migrations 폴더에 있는 마이그레이션 파일들을 읽어서 DB에 적용합니다.
 npx prisma migrate deploy
