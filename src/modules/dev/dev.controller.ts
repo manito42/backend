25366ef8cba0b44b7435710fd4bf97ea06b277d6
myukang <myunghwan0421@gmail.com>
2023-08-25 19:08:14 +0900

    :bulb: [Refactor] : Add Comment
    ## example
    
    ```ts
    import { DynamicModule, Global, Module } from '@nestjs/common';
    import { AuthModule } from './auth/auth.module';
    import { FakeauthModule } from './fakeauth/fakeauth.module';
    
    @Global()
    @Module({})
    export class DynamicAuthModule {
      static forRoot(): DynamicModule {
        const isFakeAuthEnabled =
          process.env.NODE_ENV === 'development'
            ? true
            : process.env.NODE_ENV === 'test'
            ? true
            : false;
    
        const imports = isFakeAuthEnabled
          ? [FakeauthModule, AuthModule]
          : [AuthModule];
    
        return {
          module: DynamicAuthModule,
          imports,
        };
      }
    }
    ```

--- src/modules/dev/dev.controller.ts
+++ src/modules/dev/dev.controller.ts
@@ -5,11 +5,19 @@ import { DevService } from './dev.service';
 
 @Controller('dev')
 export class DevController {
-  constructor(private readonly devService: DevService, private readonly userService: UserService) {}
+  constructor(
+    private readonly devService: DevService,
+    private readonly userService: UserService,
+  ) {}
 
   @Get('/login/:id')
   async loginDev(@Param('id') id: number) {
-    if (process.env.NODE_ENV !== 'dev') throw new UnauthorizedException();
+    /**
+     * @suggest
+     * - use dynamic module
+     * */
+    if (process.env.NODE_ENV !== 'dev' && process.env.NODE_ENV !== 'test')
+      throw new UnauthorizedException();
     const user = await this.userService.findById(id);
     if (!user) throw new BadRequestException();
     const accessToken = await this.devService.createToken(user);
