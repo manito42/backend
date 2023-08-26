93019f386662bb06f20d835c38dbefeab737d8e7
Myukang <76278794+koreanddinghwan@users.noreply.github.com>
2023-08-26 16:52:23 +0900

    Feature/reservation (#37)
    * :truck: [Chore]: rename test => e2e
    
    * :memo: [Chore]: swagger
    
    update swagger api url
    
    * :sparkles: [Feature]: reservation repository
    
    * :white_check_mark: [Test]: testing env file
    
    * :memo: [Chore]: Api Docs
    
    - Api Tag로 Api 분리구분
    
    <img width="553" alt="스크린샷 2023-08-24 20 25 20" src="https://github.com/manito42/backend/assets/76278794/0068c9ce-d643-428a-9c09-9a98bea791f9">
    
    * :arrow_up: [Chore]: upgrade deps
    
    1. NestJs9 => NestJs10
    
    2. Prisma4.2 =>5.2
    
    `prisma.service.ts`에서 enableShutdownHooks삭제
    -> binary engine사용하지 않으므로, beforeExit은 작동하지 않음.
    
    * :bulb: [Refactor] : Add Comment to Create
    
    * :bulb: [Refactor] : Add Comment
    
    ## example
    
    ```ts
    import { DynamicModule, Global, Module } from '@nestjs/common';
    import { AuthModule } from './auth/auth.module';
    import { FakeauthModule } from './fakeauth/fakeauth.module';
    
    @Global()
    @Module({})
    export class DynamicAuthModule {
      static forRoot(): DynamicModule {
        const isFakeAuthEnabled =
          process.env.NODE_ENV === 'development'
            ? true
            : process.env.NODE_ENV === 'test'
            ? true
            : false;
    
        const imports = isFakeAuthEnabled
          ? [FakeauthModule, AuthModule]
          : [AuthModule];
    
        return {
          module: DynamicAuthModule,
          imports,
        };
      }
    }
    ```
    
    * :recycle: [Refactor]: Repository Pattern
    
    - 컨트롤러 레이어와 서비스 레이어의 트랜잭션 코드를 레포지토리 안으로 밀어넣는 것이 어떨까 싶습니다.
    
    * :white_check_mark: [Test] : Test Init
    
    * :card_file_box: [Feature]: Reservation Status
    
    * :white_check_mark: [Test]: Request Cancel
    
    * :recycle: [Refactor]: Repository
    
    ReservationRepository:
    transaction code들 Repository로 이동
    
    * :bug: [Fix]: Cancel Reservation
    
    reservation status Cancel
    
    * :recycle: [Refactor]: Code Relocate
    
    relocate complete reservation mentee
    
    * :recycle: [Refactor]: Code Relocate
    
    relocate complete reservation mentee
    
    * :card_file_box: [Feature] : Reservation Status Change
    
    PENDING => MENTEE_FEEDBACK으로 변경됨.
    
    * :white_check_mark: [Test]: reservation.e2e-spec.ts
    
    1. 예약 프로세스 플로우 별 E2E 테스트 완료
    
    * Delete .env.test
    
    * Update dev.controller.ts
    
    * :test_tube: [Test]: add failing test
    
    - 없는 reservation cancel하는 경우에 대한 테스트.
    
    findUniqueOrThrow는 database계층 에러만 던지기때문에 prismaClientException이 발생하고 이후 http exception filter에서 단순하게 400으로만 변환.
    
    ```sh
    [Nest] 184  - 08/26/2023, 4:13:39 PM   ERROR [PrismaClientExceptionFilter] P2025
     FAIL  e2e/reservation/reservation.e2e-spec.ts (9.984 s)
    ```
    
    * Delete migration.sql
    
    * Delete migration.sql
    
    * :white_check_mark: [Test]: afterAll
    
    prisma disconnection 삭제
    app.close => afterAll로 이동
    
    * :memo: [Docs]: update readme.md
    
    test방법 명시

--- src/models/reservation/reservation.module.ts
+++ src/models/reservation/reservation.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { ReservationController } from './reservation.controller';
 import { ReservationService } from './reservation.service';
 import { PrismaModule } from '../../database/services/prisma.module';
+import { ReservationRepository } from '../../database/repository/reservation.repository';
+import { JwtGuard } from '../../common/guards/jwt/jwt.guard';
+import { JwtStrategy } from '../../common/guards/jwt/jwt.strategy';
+import { JwtConfigModule } from '../../config/jwt/config.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, JwtConfigModule],
   controllers: [ReservationController],
-  providers: [ReservationService],
+  providers: [ReservationService, ReservationRepository, JwtGuard, JwtStrategy],
 })
 export class ReservationModule {}
