7ff1d3823942ae9251627a83bdfe0d5e4c71f820
Photogrammer <khsj15@khu.ac.kr>
2023-08-27 20:10:45 +0900

    fix: jestSeeder.ts
    - 더 이상 seeder 를 unit test 에서 사용하지 않아, 주석처리함.

--- src/database/seeders/jestSeeder.ts
+++ src/database/seeders/jestSeeder.ts
@@ -2,5 +2,5 @@ const { execSync } = require('child_process');
 
 module.exports = async () => {
   // Assuming seeder.ts is located at the root of the project
-  execSync('ts-node src/database/seeders/seeder.ts', { stdio: 'inherit' });
+  // execSync('ts-node src/database/seeders/seeder.ts', { stdio: 'inherit' });
 };
