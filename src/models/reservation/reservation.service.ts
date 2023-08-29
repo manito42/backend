8dfc2625ae45a54a13e7328646b219e4ec739f0c
koreanddinghwan <myunghwan0421@gmail.com>
2023-08-29 20:32:13 +0900

    :recycle: [Refactor]: repository pattern
    - reservation 관련 처리 reservation repository로 database access 계층을 추가했습니다.

--- src/models/reservation/reservation.service.ts
+++ src/models/reservation/reservation.service.ts
@@ -1,7 +1,6 @@
-import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../../database/services/prisma.service';
 import { ReservationGetResponseDto } from './dto/response/reservationGetResponse.dto';
-import { ReservationSelectQuery } from './queries/reservationSelect.query';
 import { ReservationCreatePayloadDto } from './dto/request/reservationCreatePayload.dto';
 import {
   ReservationCompleteAsMenteePayloadDto,
@@ -9,9 +8,15 @@ import {
   ReservationUpdatePayloadDto,
 } from './dto/request/reservationUpdatePayload.dto';
 import { GetReservationQueryDto } from './dto/request/reservationQuery.dto';
-import { getReservationsWhereQuery } from './queries/getReservationsWhereQuery';
 import { ReservationRepository } from '../../database/repository/reservation.repository';
 import { EventEmitter2 } from '@nestjs/event-emitter';
+import { UserRole } from '@prisma/client';
+import {
+  RESERVATION_ACCEPT,
+  RESERVATION_CANCEL,
+  RESERVATION_MENTEE_COMPLETION,
+  RESERVATION_REQUEST,
+} from '../../common/constants/notification.event';
 
 @Injectable()
 export class ReservationService {
@@ -21,21 +26,27 @@ export class ReservationService {
     private readonly eventEmitter: EventEmitter2,
   ) {}
 
-  async findMany(query: GetReservationQueryDto): Promise<Array<ReservationGetResponseDto>> {
-    const { category_id, hashtag_id, take, page } = query;
-    return this.prisma.reservation.findMany({
-      take: take,
-      skip: page * take,
-      where: getReservationsWhereQuery(hashtag_id, category_id),
-      select: ReservationSelectQuery,
-    });
+  async findManyReservation(
+    query: GetReservationQueryDto,
+  ): Promise<Array<ReservationGetResponseDto>> {
+    return await this.reservationRepository.findMany(query);
   }
 
-  async findById(id: number): Promise<ReservationGetResponseDto> {
-    return this.prisma.reservation.findUnique({
-      where: { id: id },
-      select: ReservationSelectQuery,
-    });
+  async findReservationById(
+    id: number,
+    role: string,
+    userId: number,
+  ): Promise<ReservationGetResponseDto> {
+    const reservation = await this.reservationRepository.findById(id);
+
+    if (!reservation) throw new NotFoundException('not exist reservation');
+    if (
+      role !== UserRole.ADMIN &&
+      userId !== reservation.menteeId &&
+      userId !== reservation.mentorId
+    )
+      throw new UnauthorizedException();
+    return reservation;
   }
 
   /**
@@ -46,62 +57,27 @@ export class ReservationService {
    * @param payload: ReservationCreatePayloadDto
    * @returns ReservationGetResponseDto
    * */
-  async create(payload: ReservationCreatePayloadDto): Promise<ReservationGetResponseDto> {
-    const { menteeId, mentorId } = payload;
-    if (menteeId === mentorId) throw new BadRequestException('can not reserve myself');
-    const mentorProfile = await this.prisma.mentorProfile.findUnique({
-      where: { userId: mentorId },
-    });
-    if (!mentorProfile || mentorProfile.isHide)
-      throw new BadRequestException('requested mentorID is not available');
-    const existMentoringCount = await this.prisma.reservation.count({
-      where: {
-        mentorId: mentorId,
-        menteeId: menteeId,
-        OR: [{ status: 'ACCEPT' }, { status: 'REQUEST' }],
-      },
-    });
-    if (existMentoringCount !== 0) throw new ConflictException('already exists active reservation');
+  async createReservation(
+    payload: ReservationCreatePayloadDto,
+  ): Promise<ReservationGetResponseDto> {
+    const createResult = await this.reservationRepository.create(payload);
 
-    const result = await this.prisma.reservation.create({
-      data: {
-        menteeId: menteeId,
-        mentorId: mentorId,
-        categoryId: payload.categoryId,
-        requestMessage: payload.requestMessage,
-        hashtags: {
-          connect: payload.hashtags,
-        },
-      },
-      select: ReservationSelectQuery,
-    });
+    const mentor = await this.prisma.user.findUnique({ where: { id: createResult.mentorId } });
+    const mentee = await this.prisma.user.findUnique({ where: { id: createResult.menteeId } });
 
     this.eventEmitter.emit(RESERVATION_REQUEST, {
       mentor,
       mentee,
-      reservation: result,
+      reservation: createResult,
     });
-    return result;
+    return createResult;
   }
 
-  async update(
+  async updateReservation(
     id: number,
     payload: ReservationUpdatePayloadDto,
   ): Promise<ReservationGetResponseDto> {
-    return this.prisma.reservation.update({
-      where: {
-        id: id,
-      },
-      data: {
-        requestMessage: payload.requestMessage,
-        status: payload.status,
-        categoryId: payload.categoryId,
-        hashtags: {
-          set: payload.hashtags,
-        },
-      },
-      select: ReservationSelectQuery,
-    });
+    return await this.reservationRepository.update(id, payload);
   }
 
   async cancelReservation(reservationId: number, userId: number, role: string) {
