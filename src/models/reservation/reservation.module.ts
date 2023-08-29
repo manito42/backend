b63c6eddf324750e07eb8dd62b55c1a19d1c53c8
koreanddinghwan <myunghwan0421@gmail.com>
2023-08-29 20:18:47 +0900

    :recycle: [Refactor]: ReservationModule
    - EventEmitterModule : 종속성 추가
    - EventEmitter2: service에서 사용. provider추가

--- src/models/reservation/reservation.module.ts
+++ src/models/reservation/reservation.module.ts
@@ -6,11 +6,11 @@ import { ReservationRepository } from '../../database/repository/reservation.rep
 import { JwtGuard } from '../../common/guards/jwt/jwt.guard';
 import { JwtStrategy } from '../../common/guards/jwt/jwt.strategy';
 import { JwtConfigModule } from '../../config/jwt/config.module';
-import { EventEmitterModule } from '@nestjs/event-emitter';
+import { EventEmitter2, EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [PrismaModule, JwtConfigModule, EventEmitterModule],
+  imports: [PrismaModule, JwtConfigModule, EventEmitterModule.forRoot()],
   controllers: [ReservationController],
-  providers: [ReservationService, ReservationRepository, JwtGuard, JwtStrategy],
+  providers: [ReservationService, ReservationRepository, JwtGuard, JwtStrategy, EventEmitter2],
 })
 export class ReservationModule {}
