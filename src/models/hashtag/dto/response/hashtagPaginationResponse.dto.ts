1dcff704e2efae9794bca8683975d971426b0ac1
myukang <myunghwan0421@gmail.com>
2023-09-13 14:17:19 +0900

    :recycle: [Refactor]: use PaginationInfoDto

--- src/models/hashtag/dto/response/hashtagPaginationResponse.dto.ts
+++ src/models/hashtag/dto/response/hashtagPaginationResponse.dto.ts
@@ -1,13 +1,8 @@
 import { IPaginationResponse } from 'src/common/interfaces/api/common/IPaginationResponse.interface';
+import { PaginationInfoDto } from 'src/common/interfaces/api/common/PaginationInfoDto.interface';
 import { HashtagGetResponseDto } from './hashtagGetResponse.dto';
 
 export class HashtagPaginationResponseDto implements IPaginationResponse<HashtagGetResponseDto> {
   content: HashtagGetResponseDto[];
-  page: {
-    take: number;
-    page: number;
-    totalPage: number;
-    currentPage: number;
-    isLast: boolean;
-  };
+  page: PaginationInfoDto;
 }
--- src/models/menteeFeedback/dto/response/menteeFeedbackPaginationResponse.dto.ts
+++ src/models/menteeFeedback/dto/response/menteeFeedbackPaginationResponse.dto.ts
@@ -1,15 +1,10 @@
 import { IPaginationResponse } from 'src/common/interfaces/api/common/IPaginationResponse.interface';
+import { PaginationInfoDto } from 'src/common/interfaces/api/common/PaginationInfoDto.interface';
 import { MenteeFeedbackResponseDto } from './menteeFeedbackResponse.dto';
 
 export class MenteeFeedbackPaginationResponseDto
   implements IPaginationResponse<MenteeFeedbackResponseDto>
 {
   content: MenteeFeedbackResponseDto[];
-  page: {
-    take: number;
-    page: number;
-    totalPage: number;
-    currentPage: number;
-    isLast: boolean;
-  };
+  page: PaginationInfoDto;
 }
