525c71e63b45e3397136797280c09308bbb95c4e
Photogrammer <khsj15@khu.ac.kr>
2023-09-14 20:58:53 +0900

    :bug: [Fix]: migration 과정에서 zip 명령어가 symbolic link 를 무시하는 문제를 해결함.

--- .github/scripts/startServer.sh
+++ .github/scripts/startServer.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
+logfile="/tmp/server-deploy.log"
+exec > $logfile 2>&1
+
 echo "change directory"
 cd /home/ubuntu
 
-npx prisma migrate deploy > /tmp/migrate.log
+npx prisma migrate deploy
 
 echo "Starting server..."
 npm run start:prod
