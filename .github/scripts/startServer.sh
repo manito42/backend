8b833ccb796868a90e97d8a2d262943fa9d0c5e2
myukang <myunghwan0421@gmail.com>
2023-09-18 17:17:56 +0900

    :rocket: [Fix]: logfile 경로 수정
    - 배포실패하면 서버가 종료되어 배포실패 로그가 남지 않음.
    - 마운트한 efs에 저장되도록 변경.

--- .github/scripts/startServer.sh
+++ .github/scripts/startServer.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Error 발생시 중단한다.
 set -e
-logfile="/tmp/server-deploy.log"
+logfile="/var/log/api-server/deploy/server-deploy$(date +%s).log"
 exec > $logfile 2>&1
 
 echo "change directory"
