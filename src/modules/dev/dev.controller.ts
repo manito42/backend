96d781a79aa66cdbfe5611f4207e304f792b96d5
koreanddinghwan <myunghwan0421@gmail.com>
2023-09-29 22:28:38 +0900

    :sparkles: [Feat]: database에 없어도 사용자 만들어서 토큰 리턴하도록 변경

--- src/modules/dev/dev.controller.ts
+++ src/modules/dev/dev.controller.ts
@@ -1,21 +1,16 @@
-import { BadRequestException, Controller, Get, Param, UnauthorizedException } from '@nestjs/common';
-import { UserService } from '../../models/user/user.service';
+import { Controller, Get, Param, UnauthorizedException } from '@nestjs/common';
 import * as process from 'process';
 import { DevService } from './dev.service';
 
 @Controller('dev')
 export class DevController {
-  constructor(
-    private readonly devService: DevService,
-    private readonly userService: UserService,
-  ) {}
+  constructor(private readonly devService: DevService) {}
 
   @Get('/login/:id')
   async loginDev(@Param('id') id: number) {
     if (process.env.NODE_ENV !== 'dev' && process.env.NODE_ENV !== 'test')
       throw new UnauthorizedException();
-    const user = await this.userService.findById(id);
-    if (!user) throw new BadRequestException();
+    const user = await this.devService.createOrGetUser(id);
     const accessToken = await this.devService.createToken(user);
     return {
       accessToken,
